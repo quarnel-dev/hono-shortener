97772cd06f543a2ca858e3049c34ecd718896107
quarnel-dev <quarnel.dev@gmail.com>
2026-08-25 02:52:30 +0300

    feat(api): add custom 404 and 500 JSON error handlers

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,15 @@ import redirect from './routes/redirect.route'
 
 const app = new Hono()
 
+app.notFound((ctx) => {
+  return ctx.json({ error: 'Not Found' }, 404)
+})
+
+app.onError((err, c) => {
+  console.error(`[ Server Error ]: ${err.message}`)
+  return c.json({ error: 'Internal Server Error' }, 500)
+})
+
 app.route('/links', links)
 
 app.get(
