95cec06a9c5fa638eec766b3a55005dfbcb824d0
quarnel-dev <quarnel.dev@gmail.com>
2026-08-25 02:58:32 +0300

    refactor(app): cleanup entrypoint, update dependencies and setup production configuration

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,41 @@ import links from './routes/links.route'
 import redirect from './routes/redirect.route'
 
 const app = new Hono()
+const isDocsEnabled = process.env.NODE_ENV !== 'production'
+
+app.route('/links', links)
+
+if (isDocsEnabled) {
+  app.get(
+    '/openapi',
+    openAPIRouteHandler(app, {
+      documentation: {
+        info: {
+          title: 'URL Shortener API',
+          version: '1.0.0',
+        },
+        servers: [
+          {
+            url: 'http://localhost:3000',
+            description: 'Local development server',
+          },
+        ],
+      },
+    })
+  )
+
+  app.get(
+    '/scalar',
+    Scalar({
+      theme: 'saturn',
+      spec: {
+        url: '/openapi',
+      },
+    })
+  )
+}
+
+app.route('/', redirect)
 
 app.notFound((ctx) => {
   return ctx.json({ error: 'Not Found' }, 404)
@@ -15,37 +50,5 @@ app.onError((err, c) => {
   console.error(`[ Server Error ]: ${err.message}`)
   return c.json({ error: 'Internal Server Error' }, 500)
 })
-
-app.route('/links', links)
-
-app.get(
-  '/openapi',
-  openAPIRouteHandler(app, {
-    documentation: {
-      info: {
-        title: 'URL Shortener API',
-        version: '1.0.0',
-      },
-      servers: [
-        {
-          url: 'http://localhost:3000',
-          description: 'Local development server',
-        },
-      ],
-    },
-  })
-)
-
-app.get(
-  '/scalar',
-  Scalar({
-    theme: 'saturn',
-    spec: {
-      url: '/openapi',
-    },
-  })
-)
-
-app.route('/', redirect)
 
 export default app
